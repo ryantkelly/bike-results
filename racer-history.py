39c86427f89977499c1690238e0efdaf60a4b9a5
Ryan Kelly <ryantkelly@gmail.com>
2018-08-03 11:37:24 -0400

    Uses arguments like a real person.

--- racer-history.py
+++ racer-history.py
@@ -1,5 +1,7 @@
-import requests, sys, csv, json
+import requests, sys, csv, json, argparse
 from bs4 import BeautifulSoup
+from pprint import pprint
+
 
 # todo: 
 #	write actual error handling I guess
@@ -13,16 +15,29 @@ def is_number(s):
 	except ValueError:
 		return False
 
+parser = argparse.ArgumentParser('Compile racer history for announcing')
+parser.add_argument('--years', help="Comma-separated list of years to include", required=True)
+parser.add_argument('--type', help="Event type", choices=['road','cross'], required=True)
+parser.add_argument('--predictor_depth', help="Depth of predicted finish", default=9)
+parser.add_argument('--results_depth', help="Depth of results", default=4)
+parser.add_argument('--event_id', help="BikeReg Event ID", required=True)
 
+args = parser.parse_args()
 
-payload = {'url': 'https://www.bikereg.com/Confirmed/36030'} # Enter the confirmed riders URL from BikeReg to send to Cross Results
-validYears = [str(2016), str(2017)] # Define the years of results you want to get - here, 2015 and 2016
-predictorDepth = 9 # 0-indexed of the max predicting placing you care about - default is 4, therefore the top 5 predicted's riders results will be returned
-maxPlacing = 4 # 0-indexed. Max placing for riders results you care about. So, rider's top five results returned.
+payload = {'url': 'https://www.bikereg.com/Confirmed/'+args.event_id}
+valid_years = [y.strip(' ') for y in args.years.split(',')] # Define the years of results you want to get - here, 2015 and 2016
+predictor_depth = args.predictor_depth
+max_placing = args.results_depth
+results_type = args.type
 
-predictor = 'https://www.crossresults.com/predictor.aspx'
-historyUrl = 'https://www.crossresults.com/racer/'
-ignore = ['WAITLIST', 'Tent', 'tent', 'Wait', 'Club', 'Expo', 'Credit', 'Parking', 'Preview', 'Pre-ride'] #words in categories you want to ignore
+if results_type == "road":
+	predictor = 'https://www.road-results.com/predictor.aspx'
+	historyUrl = 'https://www.road-results.com/racer/'
+elif results_type == "cross":
+	predictor = 'https://www.crossresults.com/predictor.aspx'
+	historyUrl = 'https://www.crossresults.com/racer/'
+
+ignore = ['WAITLIST', 'Tent', 'tent', 'Wait', 'Club', 'Expo', 'Credit', 'Parking', 'Preview', 'Pre-ride', 'Intro'] #words in categories you want to ignore
 
 r = requests.get(predictor, params=payload)
 if r.status_code != 200:
@@ -37,34 +52,70 @@ for category in soup.find_all('span', class_='categoryname'):
 	writer = csv.writer(open('output/'+category.text.replace('/', '-').replace(':', '-')+'.csv', 'w'))
 	payload['cat'] = category['raceid']
 	r = requests.get(predictor, params=payload)
-	j = json.loads(r.text)
 	sort = {}
-	for racer in j:
-		sort[racer['points']] = [racer['firstname']+' '+racer['lastname'], racer['id']]
+	
+	if results_type == "cross":
+		j = r.json()
+		for racer in j:
+			sort[racer['points']] = [racer['firstname']+' '+racer['lastname'], racer['id']]
+	elif results_type == "road":
+		p_soup = BeautifulSoup(r.text,'html.parser')
+		for row in p_soup.find_all('tr', class_=['datarow1','datarow2','datarow3']):
+			cell = row.find_all('td')
+			l = []
+			try:
+				_id = cell[0].find('a')['href'].split('rID=')[1]
+			except TypeError:
+				_id = ""
+			sort[cell[2].text.strip()] = [cell[0].text.split('. ')[1], _id]
 	c = 0
 	for key in sorted(sort):
 		history_request = requests.get(historyUrl+str(sort[key][1]))
+		
 		if history_request.status_code != 200:
-			print('whoops' + r.status_code)
+			print('whoops' + str(r.status_code))
 			break
 		history_soup = BeautifulSoup(history_request.text, 'html.parser')
-		yc = 0
-		for year in history_soup.find_all('div', class_='headerrow1'):
-			shortyear = year.text[:5].strip()
-			if shortyear in validYears:
-				table = history_soup.find_all('table', class_='datatable1')[yc]
-				for tr in table.find_all('tr', class_=['datarow2', 'datarow1']):
-					td = tr.find_all('td')
-					place = td[4].text.strip()
-					if is_number(place) and int(place) <= maxPlacing:
-						# category.text
-						arr = [sort[key][0], shortyear, td[1].text.strip(), td[2].text.strip(), place]
-						try:
-							writer.writerow(arr)
-						except UnicodeEncodeError:
-							print("Unicode error on page "+historyUrl+str(sort[key][1]))
-			yc = yc + 1		
 		
+		if results_type == "cross":
+			yc = 0
+			for year in history_soup.find_all('div', class_='headerrow1'):
+				shortyear = year.text[:5].strip()
+				if shortyear in valid_years:
+					table = history_soup.find_all('table', class_='datatable1')[yc]
+					for tr in table.find_all('tr', class_=['datarow3', 'datarow2', 'datarow1']):
+						td = tr.find_all('td')
+						place = td[4].text.strip()
+						if is_number(place) and int(place) <= max_placing:
+							# category.text
+							arr = [sort[key][0], shortyear, td[1].text.strip(), td[2].text.strip(), place]
+							try:
+								writer.writerow(arr)
+							except UnicodeEncodeError:
+								print("Unicode error on page "+historyUrl+str(sort[key][1]))
+				yc = yc + 1	
+		elif results_type == "road":
+			yc = 0
+			years = history_soup.find_all('div', class_='monthContent')
+			for header in history_soup.find_all('a', class_='expandMonth'):
+				shortyear = header.text.split(' - ')[0].strip()
+				if shortyear in valid_years:
+					table = years[yc]
+					for tr in table.find_all('tr', class_=['datarow3', 'datarow2', 'datarow1']):
+						td = tr.find_all('td')
+						try:
+							place = td[4].text.strip()
+						except IndexError:
+							pprint(tr)
+						if is_number(place) and int(place) <= max_placing:
+							# category.text
+							arr = [sort[key][0], shortyear, td[1].text.strip(), td[2].text.strip(), place]
+							try:
+								writer.writerow(arr)
+							except UnicodeEncodeError:
+								print("Unicode error on page "+historyUrl+str(sort[key][1]))
+				yc+=1
+
 		c = c + 1
-		if c > predictorDepth:
+		if c > predictor_depth:
 			break
